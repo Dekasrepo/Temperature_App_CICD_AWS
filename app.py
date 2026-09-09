296ee2202dc0f99bfa4d16ea9b7596af222cdd96
jane24 <deka.obikwelu@outlook.com>
2026-09-09 21:54:10 +0100

    applied Lint corrections

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def health():
     return jsonify(status="ok", message="Temp Converter API - Dekasrepo"), 200
+
 
 @app.route("/convert")
 def convert():
@@ -14,5 +16,7 @@ def convert():
     fahrenheit = (celsius * 9 / 5) + 32
     return jsonify(celsius=celsius, fahrenheit=fahrenheit), 200
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+    
--- test_app.py
+++ test_app.py
@@ -1,15 +1,18 @@
 from app import app
 
+
 def test_health():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+
 
 def test_convert():
     client = app.test_client()
     response = client.get("/convert?celsius=0")
     assert response.status_code == 200
     assert response.get_json()["fahrenheit"] == 32.0
+
 
 def test_convert_missing_param():
     client = app.test_client()
